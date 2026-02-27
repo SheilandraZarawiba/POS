6eb913b3e986e0db1a7683fc51e27314830c0dda
sheiyyara <sheilandrazarawiba561@gmail.com>
2026-02-27 23:10:07 +0700

    POS

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>POS Home</title>
+</head>
+<body>
+    <h1>Selamat Datang di Aplikasi POS</h1>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SalesController;
+
+// home
+Route::get('/', [HomeController::class, 'index']);
+
+// products (Prefix Category)
+Route::prefix('category')->group(function () {
+    Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
+    Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
+    Route::get('/home-care', [ProductController::class, 'homeCare']);
+    Route::get('/baby-kid', [ProductController::class, 'babyKid']);
+});
+
+// User (Route Parameter)
+Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
+
+// Sales
+Route::get('/sales', [SalesController::class, 'index']);

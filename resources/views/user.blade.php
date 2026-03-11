3731037e2e974194726d19d314d627a1b5fdb7b6
sheiyyara <sheilandrazarawiba561@gmail.com>
2026-03-11 23:46:04 +0700

    Attribute Changes

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -6,12 +6,19 @@
 </head>
 
 <body>
-    <h2>Data User</h2>
-
-    <table border="1">
+    <h1>Data User</h1>
+    <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            <td>Jumlah Pengguna</td>
-            <td>{{ $data }}</td>
+            <td>ID</td>
+            <td>Username</td>
+            <td>Nama</td>
+            <td>ID Level Pengguna</td>
+        </tr>
+        <tr>
+            <td>{{ $data->user_id }}</td>
+            <td>{{ $data->username }}</td>
+            <td>{{ $data->nama }}</td>
+            <td>{{ $data->level_id }}</td>
         </tr>
     </table>
 </body>
